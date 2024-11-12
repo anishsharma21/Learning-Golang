22292a148b18114e36fc6211275561f3bf994f4b
anishsharma21 <anisharma2103@gmail.com>
2024-11-12 15:55:56 +1100

    Basics and Syntax - created a go module that was called by another module.

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
-
+	"example/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println(quote.Go());
 	fmt.Printf("Hello world, here is a number %d\n", 5);
+	message := greetings.Hello("Anish")
+	fmt.Println(message)
 }
