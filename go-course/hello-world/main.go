fbd21a827f4d1d3decd6323736fc52c8ccb3ba16
anishsharma21 <anisharma2103@gmail.com>
2024-11-14 15:15:13 +1100

    Basics and Syntax - working with types and declarations, just simple stuff.

--- go-course/hello-world/main.go
+++ go-course/hello-world/main.go
@@ -9,6 +9,27 @@ func main() {
     greet();
     fmt.Println(math.Pi);
     farewell();
+
+    // var is simplest way to declare variables
+    // it also includes implicit typing, as is seen with height
+    var age int = 21;
+    var name string = "John";
+    var height = 5.9;
+    var height2 float32 = 5.10;
+
+    // following are inferred when I used the := symbol combination
+    count := 0;
+    message := "hi there!";
+
+    // can also declare multiple variables in one go
+    var x, y int = 2, 3;
+    newname, newage := "Bob", 34;
+
+    // constants too
+    const pi = 3.1415;
+    const greeting = "Hello world!";
+
+    fmt.Println(age, name, height, height2, count, message, newname, newage, pi, greeting, x, y);
 }
 
 func greet() {
