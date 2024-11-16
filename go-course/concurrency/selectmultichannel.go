d066041caed1d654152e2edd54603b91278ad247
anishsharma21 <anisharma2103@gmail.com>
2024-11-16 19:32:34 +1100

    Concurrency - multiple channels, sequentially handling them, was using select before, but modified it to just await each channel response sequentially.

---
+++ go-course/concurrency/selectmultichannel.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch1 := make(chan string);
+	ch2 := make(chan string);
+
+	go func() {
+		ch1 <- "Message for ch1";
+	}();
+
+	go func() {
+		ch2 <- "Message for ch2";
+	}();
+
+	msg1 := <-ch1;
+	fmt.Println(msg1);
+
+	msg2 := <-ch2
+	fmt.Println(msg2);
+}
--- go-course/concurrency/simplechannels.go
+++ go-course/concurrency/simplechannels.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func simplechannels() {
 	ch := make(chan int)
 
 	go sendData(ch);
